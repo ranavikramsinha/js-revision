5677d11af8d532a59b743fd2d29f6f14e72ca5c7
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-05 18:46:50 +0530

    comparison

---
+++ 01_basics/4_comparison.js
@@ -0,0 +1,16 @@
+console.log("2">1);
+console.log(2>"1");
+console.log("02">1);
+
+//* While comparison null converts into 0 except ==
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null > 0);
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined > 0);
+
+//* === strickly checks, also data types
+console.log(2===2);
+console.log("2"===2);

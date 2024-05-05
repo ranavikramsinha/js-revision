514112ed4fdadc3908b7c940fb419702cd412885
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-05 17:37:14 +0530

    conversion of datatypes

---
+++ 01_basics/3_conversionOperation.js
@@ -0,0 +1,67 @@
+// let score = 99
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let score = "99"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let score = "99abc"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let score = null
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let score = undefined
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let score = "Rana Vikram Sinha"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let score = true
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// *******  converting into number  *******
+// "99" => 99
+// "99abc" => NaN
+// null => 0
+// undefined => NaN
+// "Rana Vikram Sinha" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn = 0
+// let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn = "Rana"
+// let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// ****** conversion into boolean
+// 1 => true
+// 0 => false
+// "" => false
+// "Rana" => true
+
+let someNumber = 100
+
+let stringNumber = String (someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);

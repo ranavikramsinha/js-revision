a97a41686e3815be1bccdb34bde8cdfa159f8d18
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-05 18:24:47 +0530

    conversion of operation

--- 01_basics/3_conversionOperation.js
+++ 01_basics/3_conversionOperation.js
@@ -60,8 +60,40 @@
 // "" => false
 // "Rana" => true
 
-let someNumber = 100
+// let someNumber = 100
 
-let stringNumber = String (someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// let stringNumber = String (someNumber)
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// ******************** Operation *************************
+
+// let value = 3
+// let negativeValue = -value
+// console.log(negativeValue);
+
+// console.log(11+11);
+// console.log(11-11);
+// console.log(11*11);
+// console.log(11**3); // means 11^3
+// console.log(11/5);
+// console.log(11%5);
+
+let str1 = "Rana"
+let str2 = " Sinha"
+let str3 = str1+str2
+console.log(str3);
+
+console.log(1 + 2); // 3
+console.log("1"+2); // 12
+console.log(1+"2"); // 12
+console.log("1"+2+2); // 122
+console.log(1+2+"2"); // 32
+
+let count1 = 1
+console.log(count1++); // print 1 first then increment 2
+console.log(++count1); // increment first then print
+
+let count2 = 1
+console.log(count2--); // print 1 first then decrement 0
+console.log(--count2); // decrement first then print

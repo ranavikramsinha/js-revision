46a6f9b6aeea18bfcbf3ea3aa5e4022e024f9784
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-05 17:06:48 +0530

    dataTypes

---
+++ 01_basics/2_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(5 + 5); // we are using node js and not a bowser
+
+let name = "Rana" // string
+let age = 18 // number
+let isLoggedIn = false // boolean
+let state; // undefined
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => (check line 8)
+// symbol => unique
+
+// object
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined

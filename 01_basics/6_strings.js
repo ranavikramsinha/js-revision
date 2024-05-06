2f07375469b4208e699423e8cbfe66784f1ffccc
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-06 19:38:01 +0530

    strings

--- 01_basics/6_strings.js
+++ 01_basics/6_strings.js
@@ -11,7 +11,7 @@ console.log(typeof str1)
 console.log(str1[0])
 console.log(str1.__proto__) //* object
 console.log(str1.length)
-console.log(str1.charAt(1))
+console.log(str1.charAt(1)) //* indexing from 0
 console.log(str1.indexOf('a')) //* if character is not present in a given string then the output will be -1
 console.log(str1.toUpperCase())
 console.log(str1.toLowerCase())
@@ -56,3 +56,37 @@ console.log(str1.startsWith("Rana",1))
 console.log(str1.startsWith("Vikram",5))
 
 console.log(str1.repeat(5))
+
+console.log(str1.padStart(25, '0'))
+console.log(str1.valueOf())
+const usingPs = str1.slice(-5)
+console.log(usingPs.padStart(str1.length, '#')) //* Read Docs again if confusion happen
+
+console.log(str1.padEnd(25, '0'))
+console.log(str1.valueOf())
+const usingPe = str1.slice(5)
+console.log(usingPe.padStart(str1.length, '#')) //* Read Docs again if confusion happen
+
+const lio = " Sinha"
+console.log(str1.lastIndexOf(lio))
+
+console.log(str1.endsWith(" Sinha"))
+console.log(str1.endsWith(" Sinha", 16)) //* 16 is not a position (original position => 17) of a in (Rana Vikram Sinha) thats why its false
+console.log(str1.endsWith(" Vikram", 11)) //* 11 is a position of m in (Rana Vikram Sinha) thats why its true
+console.log(str1.endsWith(" Sinh")) //* false
+console.log(str1.endsWith(" Sinh", 16)) //* true
+
+const name1 = "Raja"
+const name2 = "Babu"
+
+console.log(name1.concat(name2))
+console.log(name1.concat(" ",name2))
+console.log(name2.concat(name1))
+console.log(name2.concat("+",name1))
+
+const icons = "😊😂🤣"
+console.log(icons.codePointAt(1))
+console.log(icons.codePointAt(2))
+console.log(icons.codePointAt(3)) //* print unicode number of icon 🤣(56834)
+
+console.log(str1.charCodeAt(5)) //* character code 86 is equal to V (indexing from 0)

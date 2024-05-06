db945bd3b3551ce34528e3510e2280db2a7cb0f9
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-06 10:52:25 +0530

    datatypes summary and stack & heap

--- 01_basics/5_datatypes_summary.js
+++ 01_basics/5_datatypes_summary.js
@@ -14,38 +14,60 @@ let userEmail; //* let userEmail = undefined (both are same)
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId); //* id and anotherId both are different
+// console.log(id === anotherId); //* id and anotherId both are different
 
-const bigNumber = 123456789987654321n //* add n at last of a big or large number value
-console.log(typeof bigNumber);
-console.log(bigNumber);
+// const bigNumber = 123456789987654321n //* add n at last of a big or large number value
+// console.log(typeof bigNumber);
+// console.log(bigNumber);
 
 
-//* Reference (Non-Primitive Types)
+// //* Reference (Non-Primitive Types)
 
-//? Array, Objects, Functions
+// //? Array, Objects, Functions
 
-const heros = ["Rana", "Vikram", "Sinha"]; //* Array
-console.log(heros);
-console.log(typeof heros);
+// const heros = ["Rana", "Vikram", "Sinha"]; //* Array
+// console.log(heros);
+// console.log(typeof heros);
 
-let myObj = {
-    name: "Rana Vikram Sinha",
-    age: 22,
-    happy: false,
-    life: undefined,
+// let myObj = {
+//     name: "Rana Vikram Sinha",
+//     age: 22,
+//     happy: false,
+//     life: undefined,
+// }
+// console.log(myObj);
+// console.log(typeof myObj.name);
+// console.log(typeof myObj.age);
+// console.log(typeof myObj.happy);
+// console.log(typeof myObj.life);
+// console.log(typeof myObj);
+
+// const myFunction = function(){
+//     console.log("Hello World");
+// }
+// console.log(myFunction);
+// console.log(typeof myFunction);
+// console.log(myFunction());
+// console.log(typeof myFunction());
+
+//* Stack (Primitive) get a copy of an original value
+
+let myName = "Rana"
+let myAnotherName = myName
+myAnotherName = "Vikram"
+
+console.log(myName);
+console.log(myAnotherName);
+
+//* Heap (Non-Primitive) get a reference of an original value
+
+let userRana = {
+    email: "rvs@gmail.com",
+    upi: "rvs@ybl",
 }
-console.log(myObj);
-console.log(typeof myObj.name);
-console.log(typeof myObj.age);
-console.log(typeof myObj.happy);
-console.log(typeof myObj.life);
-console.log(typeof myObj);
 
-const myFunction = function(){
-    console.log("Hello World");
-}
-console.log(myFunction);
-console.log(typeof myFunction);
-console.log(myFunction());
-console.log(typeof myFunction());
+let userVikram = userRana
+userVikram.email = "svr@gmail.com"
+
+console.log(userRana.email)
+console.log(userVikram.email)

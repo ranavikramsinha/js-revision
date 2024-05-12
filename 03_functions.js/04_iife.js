a5026a710853027baff1863a30ba78722071a5f0
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-12 21:06:38 +0530

    iife

---
+++ 03_functions.js/04_iife.js
@@ -0,0 +1,18 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+(function site(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})(); // ; is important to mention for next iife
+
+( () => {
+    console.log(`DB CONNECTED TWO`)
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`)
+})("RANA");
+
+// ( function site2() {
+//     console.log(`DB CONNECTED TWO`)
+// })();

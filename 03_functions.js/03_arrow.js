2edf312821894af6a776d4abf512db9f8574368d
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-13 22:19:48 +0530

    this. uses

--- 03_functions.js/03_arrow.js
+++ 03_functions.js/03_arrow.js
@@ -1,64 +1,15 @@
-// const user = {
-//     username: "rana",
-//     price: 1099,
+const user1 = {
+    userFullName: "Rana Vikram Sinha",
+    age: 22,
 
-//     welcomeMessage: function() {
-//         console.log(`${this.username} , welcome to website`)
-//         // console.log(this)
-//     }
+    asking: function(){
+        console.log(`${this.userFullName}, How are you doing!`) //* this. use current context
+    }
+}
 
-// }
-
-// user.welcomeMessage()
-// user.username = "vikram"
-// user.welcomeMessage()
-
-// console.log(this)
-
-
-// function one(){
-//     console.log(this);
-// }
-// one()
-
-// function one(){
-//     let username = "rana"
-//     console.log(this)
-// }
-// one()
-
-// function one(){
-//     let username = "rana"
-//     console.log(this.username)
-// }
-// one()
-
-// const one = function(){
-//     let username = "rana"
-//     console.log(this.username)
-// }
-// one()
-
-// const one = () => {
-//     let username = "rana"
-//     console.log(this)
-// }
-// one()
-
-// const addTwo = (num1, num2) => {
-//     return num1 + num2
-// }
-
-// console.log(addTwo(5,5))
-
-// const addTwo = (num1, num2) => num1 + num2
-
-// console.log(addTwo(5,5))
-
-// const addTwo = (num1, num2) => (num1 + num2)
-
-// console.log(addTwo(5,5))
-
-const addTwo = (num1, num2) => ({username: "rana"})
-
-console.log(addTwo(5,5))
+user1.asking()
+console.log(user1)
+user1.userFullName = "Raja Babu"
+user1.age = 27
+user1.asking()
+console.log(user1)

879e21e3359a90437e3a9b8a40883d1a93d18421
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-16 20:14:33 +0530

    if else and operators

--- 04_control_flow/if_else.js
+++ 04_control_flow/if_else.js
@@ -41,3 +41,21 @@ else if (age>10 && age<18){
 else{
     console.log("Enjoy your childhood!")
 }
+
+let val1 = 11, val2 = 22; //* AND
+// let val1 = 1, val2 = 22; //* OR
+// let val1 = 1, val2 = 2; //* NOT
+// let val1 = 5 , val2 = 5;
+
+if (val1>5 && 15<val2){
+    console.log("AND Logical Operator")
+}
+else if(val1>33 || 20<val2){
+    console.log("OR Logical Operator")
+}
+else if(!(val1 === val2)){
+    console.log("NOT Logical Operator")
+}
+else{
+    console.log(false)
+}

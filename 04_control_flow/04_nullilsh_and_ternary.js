fa569f782cfb9ebb1b572669016f889057f7d23a
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-16 22:50:22 +0530

    nullish and ternary

---
+++ 04_control_flow/04_nullilsh_and_ternary.js
@@ -0,0 +1,19 @@
+//* Nullish Coalescing Operator (??): null , undefined
+
+let val1 = 11 ?? 22
+console.log(val1)
+
+let val2 = null ?? 33 ?? 66
+console.log(val2)
+
+let val3 = undefined ?? 55 ?? 77
+console.log(val3)
+
+//* Ternary Operator => ?
+//* condition ? true : false
+
+const name1 = "Raja"
+name1 === "Raja" ? console.log("Correct Name") : console.log("Wrong Name")
+
+const name2 = "Raja Babu"
+name2 === "Raja" ? console.log("Correct Name") : console.log("Wrong Name")

31b7b250edcf9adc372cca02322adfdd5128dbbf
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-16 19:40:47 +0530

    if else and comparison

---
+++ 04_control_flow/if_else.js
@@ -0,0 +1,24 @@
+//* if - else if - else
+
+if(true){    //* if(condition's)
+    //* this code executes because the condition is true
+}
+else{
+    //* this code will execute if upper if's not runs because of condition
+}
+
+if(false){
+    //* this code not executes because the condition is false
+}
+else if (true){
+    //* this code executes because the condition is true
+}
+else{
+    //* this code will execute if upper if's not runs because of condition
+}
+
+
+//************************ Comparison Operator's ***********************
+
+//* <, >, <=, >=, !=, ==, === (also check data types)
+

d0883b896a5b07a41aac54fdd0643f40bda6cfa8
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-17 17:53:47 +0530

    do while loop

---
+++ 05_loops/do.js
@@ -0,0 +1,15 @@
+//* do while => this loop will run at least one time if the condition not matches (see in 2nd do while loop)
+
+let val1 = 5
+
+do{
+    console.log(`Value is ${val1}`)
+    val1++
+} while (val1 <= 25)
+
+let val2 = 55
+
+do{
+    console.log(`Value is ${val2}`)
+    val2++
+} while (val2 <= 25)

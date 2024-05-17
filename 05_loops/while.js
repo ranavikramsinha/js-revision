0c773a3480c2a974eb7167f51cd2251efcebc923
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-17 17:38:02 +0530

    while loop

---
+++ 05_loops/while.js
@@ -0,0 +1,25 @@
+//* While
+
+let element1 = 0
+
+while(element1 <= 10){
+    console.log(`Element number is ${element1}`)
+    element1++
+}
+
+
+const str1 = "Raja Babu"
+let element2 = 0
+
+while(element2 < str1.length){
+    console.log(`Character in index ${element2} is ${str1[element2]}`)
+    element2++
+}
+
+const arr1 = [11, 33, 55, 77, 99]
+let element3 = 0
+
+while(element3 < arr1.length){
+    console.log(`arr1 index ${element3} has ${arr1[element3]} value`)
+    element3++
+}

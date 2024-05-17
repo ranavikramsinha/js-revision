176ec7260dac4e8541f25455f4c9e55a56859106
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-17 17:09:40 +0530

    for loop

---
+++ 05_for/for.js
@@ -0,0 +1,24 @@
+//* For
+
+for (let i=0; i<=10; i++){
+    console.log(i)
+}
+
+const str1 = "Rana Vikram Sinha"
+
+for (let i=0; i<str1.length; i++){
+    console.log(str1[i])
+}
+
+const arr1 = [11, 22, 33, 44, 55, 66]
+
+for(let i=0; i<arr1.length; i++){
+    console.log(arr1[i])
+}
+
+const arr2 = [233, 56, 255, 66, 32]
+for(let i=0; i<arr2.length; i++){
+    const element = arr2[i]
+    console.log(element)
+}
+

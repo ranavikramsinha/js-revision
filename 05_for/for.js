e182962fbe0206b6d75f0141d4a9621fcd8930e9
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-17 17:20:43 +0530

    for loop and break & continue

--- 05_for/for.js
+++ 05_for/for.js
@@ -5,13 +5,11 @@ for (let i=0; i<=10; i++){
 }
 
 const str1 = "Rana Vikram Sinha"
-
 for (let i=0; i<str1.length; i++){
     console.log(str1[i])
 }
 
 const arr1 = [11, 22, 33, 44, 55, 66]
-
 for(let i=0; i<arr1.length; i++){
     console.log(arr1[i])
 }
@@ -22,3 +20,29 @@ for(let i=0; i<arr2.length; i++){
     console.log(element)
 }
 
+
+//*********************** Break and Continue ***************************
+
+const str2 = "Raja Babu"
+for(let i=0; i<=str2.length; i++){
+    const element = str2[i]
+    if(element === " "){
+        console.log(`Space "${element}" has founded`)
+        break; //* loop will break and iteration will stop
+    }
+    else{
+        console.log(element)
+    }
+}
+
+
+const str3 = "Aryan Sinha"
+for(let i=0; i<=str2.length; i++){
+    const element = str3[i]
+    if(element === " "){
+        continue; //* Space will not print and loop will continue to next iteration
+    }
+    else{
+        console.log(element)
+    }
+}

4e2ca909719a9c8c8266ffb0ca40aef21f85d8c9
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-21 17:51:07 +0530

    learned child parent in DOM

---
+++ 07_DOM/dom2script.js
@@ -0,0 +1,27 @@
+const parent = document.querySelector(".parent")
+// console.log(parent)
+// console.log(parent.children)
+// console.log(parent.children[6].innerHTML)
+
+// for(let i=0; i<parent.children.length; i++){
+//     console.log(parent.children[i].innerHTML)
+// }
+
+parent.children[1].style.color = "#654321"
+// console.log(parent.firstElementChild)
+// console.log(parent.lastElementChild)
+
+// const firstElement = document.querySelector(".parent")
+// firstElement.innerText = "Rana Vikram Sinha"
+// console.log(firstElement)
+
+// const firstElement = document.querySelector(".day")
+// firstElement.innerText = "Rana Vikram Sinha"
+// console.log(firstElement)
+
+const day1 = document.querySelector(".day")
+// console.log(day1)
+// console.log(day1.parentElement)
+// console.log(day1.nextElementSibling)
+
+console.log("NODES: ", parent.childNodes) //* also counts one line break and texts

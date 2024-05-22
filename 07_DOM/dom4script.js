61877d5bf01a0bc459cc38b481d96e3f485addb9
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-22 18:18:33 +0530

    add elements with color using DOM

--- 07_DOM/dom4script.js
+++ 07_DOM/dom4script.js
@@ -1,11 +1,30 @@
-function optimizeAddLiElement (name){
+// function optimizeAddLiElement (name){
+//     const li = document.createElement("li")
+//     console.log(li)
+//     li.appendChild(document.createTextNode(`${name}`))
+//     document.querySelector(".names").appendChild(li)
+// }
+// optimizeAddLiElement("Rana")
+// optimizeAddLiElement("Pankaj")
+
+// function optAddElement (name){
+//     const li = document.createElement("li")
+//     li.appendChild(document.createTextNode(`${name}`))
+//     document.querySelector(".names").appendChild(li)
+// }
+
+// optAddElement("Pankaj")
+
+function optAddElementWithColor (name, color){
     const li = document.createElement("li")
-    console.log(li)
+    li.style.color = `${color}`
     li.appendChild(document.createTextNode(`${name}`))
     document.querySelector(".names").appendChild(li)
 }
-optimizeAddLiElement("Rana")
-optimizeAddLiElement("Pankaj")
+
+optAddElementWithColor("Pankaj", "Orange")
+optAddElementWithColor("Rishav", "Yellow")
+optAddElementWithColor("Rana", "Green")
 
 //* NOTE => innerHTML, textCont and innerText, Traverse all the elements in it before adding the new element's
 

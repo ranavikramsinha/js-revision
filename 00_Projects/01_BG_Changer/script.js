e33868489e2f23cf37462151f8d2965ce9236e21
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-22 20:59:13 +0530

    practice project 1

--- 00_Projects/01_BG_Changer/script.js
+++ 00_Projects/01_BG_Changer/script.js
@@ -1,23 +1,43 @@
+// const buttons = document.querySelectorAll(".button")
+// // console.log(buttons)
+// const body = document.querySelector("body")
+
+// buttons.forEach((button) => {
+//     // console.log(button)
+//     button.addEventListener("click", (event) => {
+//         // console.log(event)
+//         // console.log(event.target)
+//         if(event.target.id === "red"){
+//             body.style.backgroundColor = event.target.id
+//         }
+//         else if(event.target.id === "cyan"){
+//             body.style.backgroundColor = event.target.id
+//         }
+//         else if(event.target.id === "orange"){
+//             body.style.backgroundColor = event.target.id
+//         }
+//         else if(event.target.id === "brown"){
+//             body.style.backgroundColor = event.target.id
+//         }
+//     })
+// })
+
 const buttons = document.querySelectorAll(".button")
-// console.log(buttons)
-const body = document.querySelector("body")
+const wholeBody = document.querySelector("body")
 
 buttons.forEach((button) => {
-    // console.log(button)
     button.addEventListener("click", (event) => {
-        // console.log(event)
-        // console.log(event.target)
         if(event.target.id === "red"){
-            body.style.backgroundColor = event.target.id
+            wholeBody.style.backgroundColor = event.target.id
         }
         else if(event.target.id === "cyan"){
-            body.style.backgroundColor = event.target.id
+            wholeBody.style.backgroundColor = event.target.id
         }
         else if(event.target.id === "orange"){
-            body.style.backgroundColor = event.target.id
+            wholeBody.style.backgroundColor = event.target.id
         }
         else if(event.target.id === "brown"){
-            body.style.backgroundColor = event.target.id
+            wholeBody.style.backgroundColor = event.target.id
         }
     })
 })

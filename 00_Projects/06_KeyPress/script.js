2ab921a1e16b091d8964eb893be7b1ba4f5db91d
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-05-28 22:06:22 +0530

    project 6 keyPress

---
+++ 00_Projects/06_KeyPress/script.js
@@ -0,0 +1,18 @@
+const insert = document.getElementById("insert")
+
+window.addEventListener("keydown", (event) => {
+    insert.innerHTML = `
+    <div class="color">
+    <table>
+        <tr>
+            <th>Key</th>
+            <th>Code</th>
+        </tr>
+        <tr>
+            <td>${event.key === " " ? "Space" : event.key}</td>
+            <td>${event.code}</td>
+        </tr>
+    </table>
+    </div>
+    `
+})

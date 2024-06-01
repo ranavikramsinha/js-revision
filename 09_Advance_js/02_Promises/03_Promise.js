34d6cf29bf27ecb5d9dcbc91ab1d294fd27aa885
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-06-01 09:35:11 +0530

    practice how to resolve callback hell using promise and also use setTimeout

--- 09_Advance_js/02_Promises/03_Promise.js
+++ 09_Advance_js/02_Promises/03_Promise.js
@@ -21,29 +21,62 @@
 //     });
 //   })
 
+// function makeHttpRequest(method, url){
+//     const xhr = new XMLHttpRequest()
+//     xhr.responseType = "json"
+
+//     const promise1 = new Promise((resolve, reject) => {
+//         xhr.addEventListener("load", () => {
+//             resolve(xhr.response)
+//         })
+        
+//         xhr.addEventListener("error", () => {
+//             reject("Request Failed")
+//         })
+//     })
+
+//     xhr.open(method, url)
+//     xhr.send()
+
+//     return promise1
+// }
+
+// makeHttpRequest("GET", "https://dummyjson.com/users")
+// .then((usersData) => makeHttpRequest("GET", `https://dummyjson.com/posts/user/${usersData.users[0].id}`))
+// .then((postsData) => makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`))
+// .then((commentsData) => makeHttpRequest('GET', `https://dummyjson.com/users/${commentsData.comments[0].user.id}`))
+// .then((commentsUserData) => console.log(commentsUserData))
+// .catch((err) => console.log(err))
+
 function makeHttpRequest(method, url){
     const xhr = new XMLHttpRequest()
     xhr.responseType = "json"
 
-    const promise1 = new Promise((resolve, reject) => {
+    const promise2 = new Promise((resolve,reject) => {
+
         xhr.addEventListener("load", () => {
-            resolve(xhr.response)
+            setTimeout(() => {
+                resolve(xhr.response)
+            }, 1000)
         })
-        
+
         xhr.addEventListener("error", () => {
+            setTimeout(() => {
             reject("Request Failed")
+            }, 1000)
         })
+
     })
 
     xhr.open(method, url)
     xhr.send()
 
-    return promise1
+    return promise2
 }
 
 makeHttpRequest("GET", "https://dummyjson.com/users")
 .then((usersData) => makeHttpRequest("GET", `https://dummyjson.com/posts/user/${usersData.users[0].id}`))
-.then((postsData) => makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`))
-.then((commentsData) => makeHttpRequest('GET', `https://dummyjson.com/users/${commentsData.comments[0].user.id}`))
-.then((commentsUserData) => console.log(commentsUserData))
-.catch((err) => console.log(err))
+.then((posts) => makeHttpRequest("GET", `https://dummyjson.com/comments/post/${posts.posts[0].id}`))
+.then((commentsData) => makeHttpRequest("GET", `https://dummyjson.com/users/${commentsData.comments[1].user.id}`))
+.then((commentsDataOfUser) => console.log(commentsDataOfUser))
+.catch((error) => console.log(error))

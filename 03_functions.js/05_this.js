7e1c9730c485a30b750fc74eb60c5278f9e649ce
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-06-04 13:42:03 +0530

    THIS Keyword in Runtiime and in Browser

--- 03_functions.js/05_this.js
+++ 03_functions.js/05_this.js
@@ -1,17 +1,4 @@
-// function getThis() {
-//     console.log(this)
-// }
-
-// const obj = {
-//     getThis,
-// }
-// getThis()
-// obj.getThis()
-
-// //* O/P => 1st - window (in browser) and global (in node,deno,bun)
-// //*        2nd - {getThis: f}
-
-const getThis = () => {
+function getThis() {
     console.log(this)
 }
 
@@ -20,6 +7,49 @@ const obj = {
 }
 getThis()
 obj.getThis()
+
+//* O/P => 1st - window (in browser) and global (in node,deno,bun)
+//*        2nd - {getThis: f}
+
+const getThis2 = () => {
+    console.log(this)
+}
+
+const obj2 = {
+    getThis2,
+}
+getThis2()
+obj2.getThis2()
+
+//* O/P => 1st - {} (window (in browser) and global (in node,deno,bun)) 
+//*        2nd - {} (window (in browser) and global (in node,deno,bun))
+
+function getThis3 () {
+    console.log(this)
+}
+
+const obj3 = {
+    getObjThis: function(){
+        console.log(this)
+    },
+}
+getThis3()
+console.log(obj3)
+
+//* O/P => 1st - window (in browser) and global (in node,deno,bun)
+//*        2nd - {getObjThis: f}
+
+const getThis4 = () => {
+    console.log(this)
+}
+
+const obj4 = {
+    getObjThis: () => {
+        console.log(this)
+    },
+}
+getThis4()
+obj4.getObjThis()
 
 //* O/P => 1st - {} (window (in browser) and global (in node,deno,bun)) 
 //*        2nd - {} (window (in browser) and global (in node,deno,bun))

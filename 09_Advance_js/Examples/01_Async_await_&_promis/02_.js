0e0a6c98675bba1b5b053d6ca7b2b8b617c65c85
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-06-05 17:31:11 +0530

    example of asyn await and promise

--- 09_Advance_js/Examples/01_Async_await_&_promis/02_.js
+++ 09_Advance_js/Examples/01_Async_await_&_promis/02_.js
@@ -31,3 +31,17 @@ setTimeout(() => {
 
 exec();
 console.log("End");
+
+//* Start (synchronous)
+//* First (synchronous inside exec)
+//* End (synchronous)
+//* Promise Resolved (microtask from myPromise.then)
+//* function above timeout (first macrotask from setTimeout)
+//* function Below timeout (second macrotask from setTimeout)
+//* Await Before Timeout (third macrotask from setTimeout)
+//* Data from fetch (microtask from await fetch and await response.json())
+//* Fetch request is initiated and the function pauses at await fetch.
+//* Other macrotasks and microtasks are processed while the fetch request is pending.
+//* Once the fetch request completes, the promise resolves, and the subsequent await response.json() continues.
+//* Data is logged as a microtask.
+//* Await After Timeout (fourth macrotask from setTimeout)
